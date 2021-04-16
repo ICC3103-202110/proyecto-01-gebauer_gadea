2a5fa9b4228545247142a118e18fada0bcffa2fe
Sofia Gadea <slgadea@miuandes.cl>
2021-04-16 12:26:29 -0400

    primer commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+#primero pregunta cantidad de jugadores
+from players import Players
+players = []
+influences = []
+posibilities = ["D","A","Ca","Co","E"]
+
+
+    
+def select_influence():
+    card = influences[0]
+    influences.pop(0)
+    return card
+
+def create_game():
+    players_num = int(input("Elija la cantidad de jugadores (3 o 4)"))
+    for i in posibilities:
+        for j in range(0,players_num):
+            influences.append(i)
+    random.shuffle(influences)
+    card1 = select_influence()
+    card2 = select_influence()
+    for i in range(0,players_num):
+        p = Players(str(i+1),card1,card2)
+        players.append(p)
+    
+
+def show_cards():
+    print("\nJugadores y cartas ")
+    for (i,_) in enumerate(players):
+        print(f"{i}: {players[i].player_number}-{players[i].influence1}-{players[i].influence2}")
+
+
+
+if __name__ == '__main__':   
+    create_game()
+
---
+++ players.py
@@ -0,0 +1,15 @@
+class Players:
+    def __init__(self,player_number,influence1,influence2):
+        self.__player_number = player_number
+        self.__incluence1 = influence1
+        self.__incluence2 = influence2
+@property
+def player_number(self):
+    return self.__player_number
+
+@property
+def influence1(self):
+    return self.__influence1
+@property
+def __influence2(self):
+    return self.__influence2
